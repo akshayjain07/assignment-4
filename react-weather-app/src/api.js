38364397de4a11938d1e862aa0d148bece6bbcff
AKSHAY JAIN <akshay.070603@gmail.com>
2024-06-06 14:18:27 +0530

    first commit

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1d2e700177msh4c26cf87040bd1bp1b3dacjsn6196900ffabf",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8bd5911adaf3335ebc2a4338c4fc9fd8"; // enter your key from openweather API
